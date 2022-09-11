230071c393b516d15188ced625c37c70c480c363
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-11 21:20:04 +0200

    promise practised in js

--- promises.js
+++ promises.js
@@ -5,7 +5,7 @@
 //3. reject (reject)
 
 //how to call a constructor by using new
-
+/*
 const MakePromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         const arrOne = ["userOne", "userTwo", "userThree"];
@@ -20,6 +20,44 @@ const MakePromise = new Promise((resolve, reject) => {
 
 //console.log(MakePromise.then()); //Promise { <pending> }
 
+
+//MakePromise.then().catch()
 MakePromise.then((result) => {
     console.log(result);
+}).catch((result) => {
+    console.log(result);
 });
+
+//Homework how this output going to work
+//console.log(MakePromise.then())
+
+*/
+
+//Async and Await
+
+const UserOne = () => {
+    return "I am UserOne";
+};
+const UserTwo = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("User Found");
+        }, 1500);
+    });
+};
+const UserThree = () => {
+    return "I am UserThree";
+};
+
+const wait = async() => {
+    let GuestOne = UserOne();
+    console.log(GuestOne);
+
+    let GuestTwo = await UserTwo(); // User Found ---is the output if it has Async and Await
+    console.log(GuestTwo); //without Async and Await...its in pending form.... Promise { <pending> }
+
+    let GuestThree = UserThree();
+    console.log(GuestThree);
+};
+
+wait();
