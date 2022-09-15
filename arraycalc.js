b9677c0e1462ae746db8ba1f2bc620a0f40605f5
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-15 15:31:03 +0200

    learned array calculation

--- arraycalc.js
+++ arraycalc.js
@@ -19,28 +19,32 @@ const minAges = Math.min(...ages);
 
 //Find the median age(one middle item or two middle items divided by two)
 let length = ages.length;
-console.log(length);
+//console.log(length);
 let median;
 
 if (length % 2 != 0) {
     median = Math.floor(length / 2);
     middleNum = ages[median];
 
-    console.log(median);
-    console.log(middleNum);
+    // console.log(median);
+    // console.log(middleNum);
 } else {
     median = Math.floor(length / 2);
     middleNum1 = ages[median - 1];
     middleNum2 = ages[median];
-    console.log(middleNum1);
-    console.log(middleNum2);
+    // console.log(middleNum1);
+    // console.log(middleNum2);
 
 }
-
-
 //Find the average age(all items divided by number of items)
+const sum = ages.reduce((a, b) => a + b, 0);
+console.log(sum); //228
+const average = sum / ages.length;
+console.log(average); //22.8
 
 //Find the range of the ages(max minus min)
+const range = maxAges - minAges;
+console.log(range); // 26 - 19 = 7
 
 
 //Compare the value of (min - average) and (max - average), use abs() method
