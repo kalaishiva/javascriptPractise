2e73c723cd5c9fda2e0e7f5bce6a677018e4506c
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-15 16:42:51 +0200

    sum of even numbers

---
+++ forLoop.js
@@ -0,0 +1,25 @@
+//28. Use for loop to iterate from 0 to 100 and print only prime numbers
+
+
+
+
+//29. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+
+let prevSum = 0;
+let evenSum = 0;
+let oddSum = 0;
+
+for (let index = 0; index <= 100; index++) {
+    if (index % 2 === 0) {
+        prevSum = evenSum;
+        evenSum = evenSum + index;
+
+        console.log(`${prevSum} + ${index} = ${evenSum}`);
+    } else {
+        prevSum = oddSum;
+        oddSum = oddSum + index;
+        console.log(`${prevSum} + ${index} = ${oddSum}`);
+
+    }
+
+}
